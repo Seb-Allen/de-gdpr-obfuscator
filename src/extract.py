1801516269b8fdf150e8e3d1ec3d5aa76aa96018
Seb <seb.allen@protonmail.com>
2025-10-09 01:20:23 +0100

    main takes json input

--- src/extract.py
+++ src/extract.py
@@ -1,39 +1,25 @@
 import boto3
+import json
 
-
-def main():
+def main(json_input):
+    s3_bucket, s3_key = file_location(json_input)
     client = s3_client()
-    csv_ingestion(client)
-    some_function()
+    file_to_obfuscate = csv_extraction(client, s3_bucket, s3_key)
+    return file_to_obfuscate
 
-
-def some_function():
-    pass
+def file_location(json_input):
+    s3_bucket = "test_bucket"
+    s3_key = "test_csv.csv"
+    return s3_bucket, s3_key
 
 def s3_client():
     return boto3.client("s3")
 
-def csv_ingestion(s3_client):
-    with open('./test/test_data/test_csv.csv', 'r') as file:
-        s3_client.put_object(
-            Bucket="nc-de-test",
-            Key="test_csv.csv",
-            Body=file.read()            
-        )
-    
-    response = s3_client.get_object(
-        Bucket="nc-de-test",
-        Key="test_csv.csv")
-    
-    assert response['ResponseMetadata']['HTTPStatusCode'] == 200
-    # print(response)
-    # print(response['Body'].read().decode('utf-8'))
-
-
-
-
-
-
+def csv_extraction(s3_client, s3_bucket, s3_key):    
+    file_to_obfuscate = s3_client.get_object(
+        Bucket=s3_bucket,
+        Key=s3_key)
+    return file_to_obfuscate
 
 if __name__ == "__main__":
     main()
