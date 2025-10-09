044498343f9f81258a2ad83a2b8c840b4561333a
Seb <seb.allen@protonmail.com>
2025-10-09 23:51:18 +0100

    include error handling at point of extraction

--- src/extract.py
+++ src/extract.py
@@ -2,10 +2,11 @@ import boto3
 import json
 from urllib.parse import urlparse
 
-def lambda_handler(event, context):
-    s3_bucket, s3_key = file_location(event)
+def extract(json_input):
+    s3_bucket, s3_key = file_location(json_input)
     client = boto3.client("s3")
     file_to_obfuscate = csv_extraction(client, s3_bucket, s3_key)
+
     return file_to_obfuscate
 
 def file_location(json_input):
@@ -19,11 +20,11 @@ def file_location(json_input):
 
     return s3_bucket, s3_key
 
-def csv_extraction(s3_client, s3_bucket, s3_key):    
-    file_to_obfuscate = s3_client.get_object(
-        Bucket=s3_bucket,
-        Key=s3_key)
-    return file_to_obfuscate
-
-# if __name__ == "__main__":
-#     main(json_input={})
+def csv_extraction(s3_client, s3_bucket, s3_key):
+    try:
+        file_to_obfuscate = s3_client.get_object(
+            Bucket=s3_bucket,
+            Key=s3_key)
+        return file_to_obfuscate
+    except Exception as e:
+        return {f'Check the s3 url supplied for file_to_obfuscate: {e}'}
