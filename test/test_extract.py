c050d35e7cc1812d7e2a0e84b1171393672d36bc
Seb <seb.allen@protonmail.com>
2025-10-09 01:26:03 +0100

    pull pytest fixtures into test and provide json input for extract function

--- test/test_extract.py
+++ test/test_extract.py
@@ -1,8 +1,12 @@
-from src.pii import some_function
+from src.extract import main
 import pytest
 
 class TestReadsS3Data:
     @pytest.mark.it("successfully pulls data file from S3")
-    def test_reads_s3():
-        response = some_function()
+    def test_reads_s3(self, csv_ingestion):
+        with open('./test/test_data/json_input.json', 'r') as file:
+            json_input = file.read()
+
+        response = main(json_input)
+        print(response)
         assert response['ResponseMetadata']['HTTPStatusCode'] == 200
