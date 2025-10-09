69305cf75561711291cc93f1ba38fe9c126fb522
Seb <seb.allen@protonmail.com>
2025-10-09 17:02:09 +0100

    add url parsing functionality to pull s3 bucket and key

--- src/extract.py
+++ src/extract.py
@@ -1,19 +1,23 @@
 import boto3
 import json
+from urllib.parse import urlparse
 
-def main(json_input):
-    s3_bucket, s3_key = file_location(json_input)
-    client = s3_client()
+def lambda_handler(event, context):
+    s3_bucket, s3_key = file_location(event)
+    client = boto3.client("s3")
     file_to_obfuscate = csv_extraction(client, s3_bucket, s3_key)
     return file_to_obfuscate
 
 def file_location(json_input):
-    s3_bucket = "test_bucket"
-    s3_key = "test_csv.csv"
-    return s3_bucket, s3_key
+    json_input_object = json.loads(json_input)
+    
+    s3_url = json_input_object['file_to_obfuscate']
+    parsed_url = urlparse(s3_url, allow_fragments=False)
+    
+    s3_bucket = parsed_url.netloc
+    s3_key = parsed_url.path.lstrip('/')
 
-def s3_client():
-    return boto3.client("s3")
+    return s3_bucket, s3_key
 
 def csv_extraction(s3_client, s3_bucket, s3_key):    
     file_to_obfuscate = s3_client.get_object(
@@ -21,5 +25,5 @@ def csv_extraction(s3_client, s3_bucket, s3_key):
         Key=s3_key)
     return file_to_obfuscate
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main(json_input={})
