388fe42c1bf0e90c68470ded748184e19dd688fe
Seb <seb.allen@protonmail.com>
2025-10-12 22:43:00 +0100

    import polars

--- src/transform.py
+++ src/transform.py
@@ -1,9 +1,12 @@
+import polars
+from io import BytesIO
+
 def transform(json_input, file_to_obfuscate):
     columns_to_delete = pii_fields(json_input)
     obfuscate(columns_to_delete, file_to_obfuscate)
 
-def pii_fields():
-    pass
+def pii_fields(json_input):
+    return ""
 
 def obfuscate(colunns_to_delete, file_to_obfuscate):
     pass
