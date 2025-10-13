a7fa19c96a5ad36848dc678bc820b3f02b4461e3
Seb <seb.allen@protonmail.com>
2025-10-13 01:50:48 +0100

    create for loop to iterate through fields to obfuscate column values

--- src/transform.py
+++ src/transform.py
@@ -1,12 +1,50 @@
-import polars
-from io import BytesIO
+import polars as pl
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 def transform(json_input, file_to_obfuscate):
-    columns_to_delete = pii_fields(json_input)
-    obfuscate(columns_to_delete, file_to_obfuscate)
+
+    fields_to_obfuscate = pii_fields(json_input)
+    response = obfuscate(fields_to_obfuscate, file_to_obfuscate)
+    return response
+
+
 
 def pii_fields(json_input):
-    return ""
+    try:
+        json_input = json.loads(json_input)
 
-def obfuscate(colunns_to_delete, file_to_obfuscate):
-    pass
+        fields_to_obfuscate = json_input['pii_fields']
+        
+        return fields_to_obfuscate
+    
+    except Exception as e:
+        return f'Could not extract PII Fields: {e}'
+
+
+
+def obfuscate(fields_to_obfuscate, file_to_obfuscate):
+    try:
+
+        df = pl.read_csv(file_to_obfuscate)
+        
+        for field in fields_to_obfuscate:
+            df = df.lazy().with_columns(pl.lit("***").alias(field))
+        
+        clean_file = df.collect()
+        
+        logger.info("File successfully retrieved")
+        
+        return {
+            'status': 'Success',
+            'clean_file': clean_file
+        }
+    
+    except Exception as e:
+        return {
+            'status': 'Failure',
+            'error': e
+        }
