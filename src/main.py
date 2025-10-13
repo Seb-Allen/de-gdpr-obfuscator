bc5ad41915e05e3e05959735d3900acefd08dfed
Seb <seb.allen@protonmail.com>
2025-10-13 01:50:20 +0100

    include error handling for transform function

--- src/main.py
+++ src/main.py
@@ -4,15 +4,22 @@ from src.load import load
 
 
 def main(json_input):
-    response = extract(json_input)
+    extraction_response = extract(json_input)
     
-    if response['status'] == 'Failure':
-        print(response)
+    if extraction_response['status'] == 'Failure':
+        print(extraction_response)
         exit()
-    else:
-        clean_file = transform(json_input, response['file_object'])
+    
+    file_to_obfuscate = extraction_response['file_object']
+    transform_response = transform(json_input, file_to_obfuscate)
 
+    if transform_response['status'] == 'Failure':
+        print(transform_response)
+        exit()
+    
+    print(transform_response['clean_file'])
 
+    load(transform_response('clean_file'))
         
     
 
