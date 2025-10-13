82365cd2969b55abbd8ad45b9009055af2aa5740
Seb <seb.allen@protonmail.com>
2025-10-13 23:08:06 +0100

    adjust error handling to return responses

--- src/main.py
+++ src/main.py
@@ -7,23 +7,20 @@ def main(json_input):
     extraction_response = extract(json_input)
     
     if extraction_response['status'] == 'Failure':
-        print(extraction_response)
-        exit()
+        return extraction_response
     
     file_to_obfuscate = extraction_response['file_object']
     transform_response = transform(json_input, file_to_obfuscate)
 
     if transform_response['status'] == 'Failure':
-        print(transform_response)
-        exit()
+        return transform_response
 
     clean_file = transform_response['clean_file']
 
     load_response = load(clean_file)
 
     if load_response['status'] == 'Failure':
-        print(load_response)
-        exit()
+        return load_response
 
     obfuscated_file_stream = load_response['buffer']
 
