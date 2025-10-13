a801b17fa0b6e72325a41924ba04e5db1cd7d78b
Seb <seb.allen@protonmail.com>
2025-10-13 21:45:24 +0100

    add logging

--- src/load.py
+++ src/load.py
@@ -1,4 +1,26 @@
+import polars as pl
+import logging
 from io import BytesIO
 
-def load():
-    pass
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+def load(clean_file):
+    try:
+        buffer = BytesIO()
+        clean_file.write_csv(buffer)
+        buffer.seek(0)
+
+        logger.info('Successfully written csv to buffer')
+
+        return {
+            'status': 'Success',
+            'buffer': buffer
+        }
+
+    except Exception as e:
+        logger.error(f'Unable to create BytesIO buffer: {e}')
+        return {
+            'status': 'Failure',
+            'error': e
+        }
