7eea1a2bc2015fc4681da1018e20145ca7b856be
Seb <seb.allen@protonmail.com>
2025-10-13 01:51:30 +0100

    TODO complete test_obfuscation

--- test/test_transform.py
+++ test/test_transform.py
@@ -1,9 +1,11 @@
-from src.transform import pii_fields
+from src.extract import extract
+from src.transform import transform, pii_fields
 import pytest
+import json
 
 class TestExtractsPiiFieldsToObfuscate:
-    @pytest.mark.it("extractss PII Fields to obfuscate from JSON input")
-    def  test_extract_pii_fields(self):
+    @pytest.mark.it("extracts PII Fields to obfuscate from JSON input")
+    def test_extract_pii_fields(self):
         json_input = {
             "file_to_obfuscate": "s3://test_bucket/test_csv.csv",
             "pii_fields": [
@@ -12,5 +14,21 @@ class TestExtractsPiiFieldsToObfuscate:
                 ]
                 }
         
+        json_input = json.dumps(json_input)
+        
         response = pii_fields(json_input)
-        assert response == ""
+        
+        assert isinstance(response, list)
+        assert response == ["name", "email_address"]
+
+class TestObfuscation:
+    @pytest.mark.it("tests obfuscation functionality")
+    def test_obfuscation(self, csv_ingestion):
+        with open('./test/test_data/json_input.json', 'r') as file:
+            json_input = file.read()
+        
+        response = pii_fields(json_input)
+        response = extract(json_input)
+        file_to_obfuscate = response['file_object']
+
+        transform(json_input, file_to_obfuscate)
